ef0050c52b47ab46e3ab6ed3591c490d2e4178db
Gino Rocca <roccagino5@gmail.com>
2023-05-26 14:48:24 +0200

    project navbar

---
+++ app.js
@@ -0,0 +1,10 @@
+const toggleBtn = document.querySelector(".nav-toggle");
+const links = document.querySelector(".nav-links");
+
+toggleBtn.addEventListener("click", function () {
+  links.classList.toggle("show-sidebar");
+});
+
+console.log(links);
+
+console.log(toggleBtn);
